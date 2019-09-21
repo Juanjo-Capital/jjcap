f463566e997dde1b6cd30b3cc82cafd935d9db9a
deansublett <deansublett@gmail.com>
2019-09-21 13:34:36 -0400

    Add comment outlining our fundamental ratios and target values.

--- strategies/momentum/fundamentals.py
+++ strategies/momentum/fundamentals.py
@@ -1,1 +1,19 @@
+'''
+Vedant, Shyon, and Dean
 
+We will "filter" our universe of tech stocks further according to the following ratios:
+
+1. Liquidity Ratios
+    a. Current Ratio = (current assets) / (current liabilities)
+        i. 3.0 < x < 20.0
+    b. Quick Ratio = (cash + marketable securities) / (current liabilities)
+        i. 3.0 < x < 20.0
+
+2. Financial Leverage Ratios
+    a. Debt-to-Equity Ratio = (total debt) / (total equity)
+        i. -0.1 < x < 0.2
+
+3. Profitability Ratios
+    a. Gross Profit Margin = (sales - COGS) / (sales)
+        i. 0.5 < x
+'''
